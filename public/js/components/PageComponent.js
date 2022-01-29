3452099791bd4d6d80a55333d3197e027ce1fa6b
Berenguer Pou <berenguer.pou@gmail.com>
2022-01-29 11:28:05 +0100

    added basic html template to PageComponent and menu structure

--- public/js/components/PageComponent.js
+++ public/js/components/PageComponent.js
@@ -9,8 +9,20 @@ class PageComponent extends Component {
 
   generateHTML() {
     this.element.innerHTML = `
-    <div class="container"><h1>Page Component Testing</h1>
+    <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="img/logo.png" alt="Beringar Pokémons Logo" class="menu__logo" />
+          <span class="fs-4">Beringar Pokémon Page</span>
+        </a>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">My Pokemons</a></li>
+        </ul>
+      </div>
     </div>
+  </header>
     `;
   }
 }
